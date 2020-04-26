f1abb6ebad56a62ee37dc5657bd39a6b2b10ffee
Tim Weber <scy@scy.name>
2020-04-26 10:01:24 +0200

    Add code to send to IoTPlotter

--- src/main.py
+++ src/main.py
@@ -14,8 +14,20 @@ wc.enable()
 
 NTPClient(sch, wc, "fritz.box")
 
-dht = DHT(DHT22(machine.Pin(15)), 10)
-dht.on_measurement(lambda event: print(event.more().last_measurement()))
+def send_to_iotplotter(feed_id, api_key, values):
+    import gc, urequests, utime
+    gc.collect()
+    data = "\n".join(["0,{1},{2}".format(key, val) for (key, val) in values.items()])
+    print(data)
+    res = urequests.post(
+        "https://iotplotter.com/api/v2/feed/{0}.csv".format(feed_id),
+        headers={"api-key": api_key},
+        data=data
+        )
+    res.close()
+
+dht = DHT(DHT22(machine.Pin(15)))
+dht.on_measurement(lambda event: send_to_iotplotter("<your_feed_id>", "<your_api_key>", event.more().last_measurement()))
 sch.create_task(dht.watch)
 
 sch.run_forever()
